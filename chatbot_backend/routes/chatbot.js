e3ac29345c9c67442befbd3dfefca92647915e98
Aeeril <benji.pruja@gmail.com>
2024-10-10 15:12:30 +0200

    presentation

--- chatbot_backend/routes/chatbot.js
+++ chatbot_backend/routes/chatbot.js
@@ -14,13 +14,13 @@ const openai = new OpenAI({
 const preMessage = "Vous êtes un assistant virtuel spécialisé dans la santé. Répondez aux questions des utilisateurs en fournissant des informations utiles et des recommandations.";
 
 // Route protégée pour interagir avec le chatbot (via ChatGPT)
-router.post('/', auth,async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { message } = req.body;
 
   try {
     // Appel à l'API OpenAI ChatGPT
     const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo', // Ou 'gpt-4o' selon votre besoin
+      model: 'gpt-3.5-turbo', // Ou 'gpt-4' selon votre besoin
       messages: [
         { role: 'system', content: preMessage }, // Ajoutez le prémessage ici
         { role: 'user', content: message },      // Le message que l'utilisateur envoie
@@ -29,14 +29,21 @@ router.post('/', auth,async (req, res) => {
       temperature: 0.7, // La créativité des réponses (0 = plus strict, 1 = plus créatif)
     });
 
-    // Envoyer la réponse de ChatGPT au client
-    res.json({ response: completion.choices[0].message.content }); // Accéder au contenu de la réponse
+    // Extraire la réponse de l'IA
+    let response = completion.choices[0].message.content;
+
+    // Vérifier si le message contient le mot "docteur"
+    if (message.toLowerCase().includes('docteur')) {
+      response += " je vous recommande le site web https://www.doctolib.fr/ pour trouver un médecin dans votre secteur";
+    }
+
+    // Envoyer la réponse au client
+    res.json({ response });
 
   } catch (error) {
     console.error('Erreur du chatbot:', error.response ? error.response.data : error.message);
     res.status(500).send('Erreur du chatbot');
   }
 });
-
 
 module.exports = router;
